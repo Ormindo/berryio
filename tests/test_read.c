24eb1ee27d1ecce441ead52708921204a33efd0c
Valentin Schneider <valentin.schneider@ensimag.grenoble-inp.fr>
2016-09-16 00:00:43 +0200

    Added pin select to test_read.c
    Program's first argument is pin number (berryllio numbering)

--- tests/test_read.c
+++ tests/test_read.c
@@ -2,13 +2,19 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
+	int pin = 2;
+	if (argc > 1) {
+		pin = strtol(argv[1], NULL, 10);
+	}
+
 	init_gpio();
-	set_pin_input(2);
+	set_pin_input(pin);
 	while(1) {
-		int val = read_pin(2);
+		int val = read_pin(pin);
 		printf("val = %i\n", val);
 		sleep(1);
 	}
